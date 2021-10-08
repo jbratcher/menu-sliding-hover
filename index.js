90401dded556651b3a018541f60c0ea487385399
Jeremy Bratcher <jeremybratcher@gmail.com>
2021-10-08 09:30:44 -0400

    feature: adds default params for createAnimatedNav function if no elementID is passed, adds block documentation for createAnimatedNav function, adds ability to change base font size, adds conditional for when no menu is detected in the DOM, reformats comments

--- index.js
+++ index.js
@@ -1,38 +1,73 @@
 /**
  * Styles and animates the hover background element
+ * CSS variables are set from the nav and link positions plus/minus a constant padding
  *
- *
+ * @param {string} elementID - ul tag id
+ * @param {number} documentBaseFontSize - the font size in px to establish the rem value (optional)
  */
 
-const createAnimatedNav = (elementId) => {
-  const navList = document.getElementById(elementId); // nav wrapper element (ul)
-  const links = Array.from(
-    navList.getElementsByClassName("nav-list-item-link")
-  ); // anchor tags or the nav lis
-  const baseFontSize = 16; // set the base font size
-  const padding = baseFontSize; // padding is equal to the font size (adjust as needed for more or less padding)
+const createAnimatedNav = (elementId = null, documentBaseFontSize = 16) => {
+  // nav wrapper element (ul)
+  // set the navElement to the passed elementId
+  // if elementId is not passed, try to select the nav > ul
+  const navElement = elementId
+    ? document.getElementById(elementId)
+    : document.querySelector("nav > ul");
+
+  // anchor tags of the nav lis
+  let links;
+
+  try {
+    links = Array.from(navElement.getElementsByTagName("a"));
+  } catch (error) {
+    console.warn(
+      "There doesn't seem to be a menu on this page. Please make sure you have a nav > ul structure to your menu or pass the id of the menu"
+    );
+    console.error(error);
+    return null;
+  }
+
+  // set the base font size used in the document;
+  const baseFontSize = documentBaseFontSize;
+
+  // padding is equal to the font size (adjust as needed for more or less padding)
+  const padding = baseFontSize;
+
+  // calculate the initial distance for setting the position of the ::after pseudo-element
   const initialDistanceInPx =
     links[0].getBoundingClientRect().x -
-    navList.getBoundingClientRect().x -
-    padding / 2; // calculate the initial distance for setting the position of the ::after psuedo-element
+    navElement.getBoundingClientRect().x -
+    padding / 2;
 
-  const pxToRem = (px) => px / baseFontSize + "rem"; // helper to convert 'naked' px values to rems (ex. 16 => '1rem')
-  navList.style.setProperty("--hover-position-x", pxToRem(initialDistanceInPx)); // init the hover position x CSS variable using the initial distance
-  navList.style.setProperty(
+  // helper to convert 'naked' px values to rems (ex. 16 => '1rem')
+  const pxToRem = (px) => px / baseFontSize + "rem";
+
+  // init the hover position x CSS variable using the initial distance
+  navElement.style.setProperty(
+    "--hover-position-x",
+    pxToRem(initialDistanceInPx)
+  );
+
+  // init the hover position y CSS variable using the initial distance
+  navElement.style.setProperty(
     "--hover-position-y",
-    pxToRem(links[0].offsetTop - padding / 8)
-  ); // init the hover position y CSS variable using the initial distance
-  navList.style.setProperty(
+    pxToRem(links[0].offsetTop - padding / 4)
+  );
+
+  // init the hover width CSS variable using the first link offset
+  navElement.style.setProperty(
     "--hover-width",
     pxToRem(links[0].offsetWidth + padding)
-  ); // init the hover width CSS variable using the first link offset
-  navList.style.setProperty(
+  );
+
+  // init the hover height CSS variable using the first link offset
+  navElement.style.setProperty(
     "--hover-height",
     pxToRem(links[0].offsetHeight + padding)
-  ); // init the hover height CSS variable using the first link offset
+  );
 
   // on hover, move the ::after element to the hovered link
-  links.forEach((link) =>
+  links.forEach((link) => {
     link.addEventListener("mouseenter", () => {
       // calculate distance hovered link is from start of container for positioning of ::after element
       let distanceFromStart = pxToRem(
@@ -40,17 +75,23 @@ const createAnimatedNav = (elementId) => {
           links[0].getBoundingClientRect().x +
           initialDistanceInPx
       );
-      navList.style.setProperty("--hover-position-x", distanceFromStart); // update hover position x CSS variable to match hovered link location
-      navList.style.setProperty(
+
+      // update hover position x CSS variable to match hovered link location
+      navElement.style.setProperty("--hover-position-x", distanceFromStart);
+
+      // update hover width CSS variable to match hovered link width with padding
+      navElement.style.setProperty(
         "--hover-width",
         pxToRem(link.offsetWidth + padding)
-      ); // // update hover width CSS variable to match hovered link width with padding
-      navList.style.setProperty(
+      );
+
+      // update hover height CSS variable to match hovered link height with padding
+      navElement.style.setProperty(
         "--hover-height",
         pxToRem(link.offsetHeight + padding)
-      ); // // update hover height CSS variable to match hovered link height with padding
-    })
-  );
+      );
+    });
+  });
 };
 
 export { createAnimatedNav };
